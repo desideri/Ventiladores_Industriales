7379a8813962ccef5edb2f5f507947c74ec5e001
is1394 <israel1394@gmail.com>
2016-06-04 16:53:46 -0500

    modify info empresa

--- wilenco/static/js/empresa.js
+++ wilenco/static/js/empresa.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $("a.show").click(function(){
-        div = $(this).attr('href');
-        $('.current').hide("blind");
-        $(div).show("blind");
+        div = $(this).attr('data');
+        id_current = $('.current').attr('id');
+        $('#'+id_current).removeClass('current');
+        $('#'+id_current).addClass('inactive');
+        $(div).removeClass('inactive');
+        $(div).addClass('current');
+        $('#'+id_current).hide('slow');
+        $(div).show('slow');
     });
 });
