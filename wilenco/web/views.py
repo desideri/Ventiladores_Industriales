026bd65abb1c9b6c6475d9f78bb15222d29c976c
Kattya Desiderio <desideri@espol.edu.ec>
2016-06-06 22:42:07 -0500

    Cambios en views.py y urls.py

--- wilenco/web/views.py
+++ wilenco/web/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from django.shortcuts import render
+"""
+ Django Views for wilenco project.
+ Autor: Wilenco
+ Nombre: views.py
+ Descripcion: Archivo que contiene las vistas del sitio web.
+ Version: 2.1
+ Fecha de Creacion: Mayo 30/2016
+ Ultima modificación: junio 06/2016
+ Ultimo modificador: Kattya Desiderio
+"""
+
+# Funcion  "index"
+# Descripcion: Funcion que hace render el index.
+# Fecha de Creacion: Junio 03/2016
+# Fecha de Modificacion: Junio 06/2016
 def index(request):
     return render(request, 'index.html', {})
 
+# Funcion  "contacto"
+# Descripcion: Funcion que hace render la pagina de Contactenos.
+# Fecha de Creacion: Junio 03/2016
+# Fecha de Modificacion: Junio 06/2016
 def contacto(request):
     return render(request, 'contact.html', {})
 
+# Funcion  "product"
+# Descripcion: Funcion que hace render la pagina de Productos.
+# Fecha de Creacion: Junio 03/2016
+# Fecha de Modificacion: Junio 06/2016
 def product(request):
     return render(request, 'product.html', {})
 
+# Funcion  "empresa"
+# Descripcion: Funcion que hace render la pagina de Empresa.
+# Fecha de Creacion: Junio 03/2016
+# Fecha de Modificacion: Junio 06/2016
 def empresa(request):
     return render(request, 'empresa.html', {})
--- wilenco/wilenco/urls.py
+++ wilenco/wilenco/urls.py
@@ -1,14 +1,27 @@
+# urls.py
 from django.conf.urls import include, url
 from django.contrib import admin
 
+"""
+ Django Urls for wilenco project.
+ Autor: Wilenco
+ Nombre: urls.py
+ Descripcion: Archivo que contiene las urls del sitio web.
+ Version: 2.1
+ Fecha de Creacion: Mayo 30/2016
+ Ultima modificación: junio 06/2016
+ Ultimo modificador: Kattya Desiderio
+"""
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'wilenco.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'web.views.index'),
-    url(r'^contacto/', 'web.views.contacto'),
-    url(r'^productos/', 'web.views.product'),
-    url(r'^empresa/', 'web.views.empresa'),
+
+    url(r'^$', 'web.views.index'), # URl para la pagina de index
+
+    url(r'^contacto/', 'web.views.contacto'), #URL para la pagina de Contactenos
+
+    url(r'^productos/', 'web.views.product'), #URL para la pagina de Productos
+
+    url(r'^empresa/', 'web.views.empresa'), #URL para la pagina de Empresa: Mision, Vision, Obras
+
 ]
