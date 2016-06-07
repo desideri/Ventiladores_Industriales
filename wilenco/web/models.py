2014cba2ba83c43eb149ea3c31a1abb3e2f2b9f0
Kattya Desiderio <desideri@espol.edu.ec>
2016-06-06 23:37:13 -0500

    Modelo Solicitud

--- wilenco/web/models.py
+++ wilenco/web/models.py
@@ -16,3 +16,18 @@ class Producto(models.Model):
     capacidad = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=100)
     imagen = models.ImageField(upload_to='img/productos', blank=True, null=True)
+
+# Clase  "Solicitud"
+# Descripcion: Clase para receptar la Solicitud de Instalacion/Mantenimiento.
+# Fecha de Creacion: Junio 03/2016
+# Fecha de Modificacion: Junio 06/2016
+class Solicitud(models.Model):
+    tipo =  models.BooleanField(default=False) #0 Mantemiento / 1 Instalacion
+    descripcion = models.CharField(max_length=50) # Descripcion que desee dar el cliente
+    fechaCreada = models.DateField()   # Fecha de la Creacion de la Solicitud
+    fechaEscojida = models.DateField() # Fecha tentativa del cliente para el Mantemiento/Instalacion
+    # Datos del Cliente
+    nombre = models.CharField(max_length=50)   # Nombre
+    apellido = models.CharField(max_length=50) # Apellido
+    cedulaCliente = models.IntegerField() # Cedula
+    
