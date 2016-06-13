80d0b4959dfe0478761f1d0f7e9cc8fc83bcf7f7
is1394 <israel1394@gmail.com>
2016-06-13 02:15:53 -0500

    removed unused imports

--- wilenco/web/views.py
+++ wilenco/web/views.py
@@ -12,7 +12,6 @@ from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_gis.filters import InBBoxFilter
 """
  Django Views for wilenco project.
  Autor: Wilenco
