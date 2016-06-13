61215afc1dbb8a85bf702df26b9fef6fa5914870
Jorge Santiago Ayala Sáenz <Jorge Santiago Ayala Senz>
2016-06-13 11:39:17 -0500

    Mejorando la legibilidad de models3.py

--- wilenco/web/models3.py
+++ wilenco/web/models3.py
@@ -1,11 +1,15 @@
+"""
+@Autor: Jorge Ayala
+@Ultima modificacion: Junio 13/2016
+Definicion de entidad Cotizador
+"""
 from django.db import models
-"""
-@Jsayala (Jorge Ayala)
-Entidad Cotizador contiene informacion relevante sobre cada el cliente que solicita una
-cotizacion, como fecha, id de cotizacion,  descripcion de obra o producto a cotizar
-"""
+
 class Cotizador(models.Model):
-    """ Modelo Cotizador """
+    """Entidad Cotizador contiene informacion relevante sobre cada
+    el cliente que solicita una cotizacion, como fecha, id de
+    cotizacion,  descripcion de obra o producto a cotizar
+    """
     cotizadorID = models.CharField(max_length=10)
     fechaDeSolicitud = models.DateField()
     nombresCliente = models.CharField(max_length=20)
@@ -13,5 +17,3 @@ class Cotizador(models.Model):
     telefonoCliente = models.CharField(max_length=15)
     mailCliente = models.CharField(max_length=50)
     descripcionObra = models.CharField(max_length=50)
-    
-	
