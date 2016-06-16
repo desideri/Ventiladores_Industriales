c285088db552f65f4593ee73d5f31568eff639d3
Kattya Desiderio <desideri@espol.edu.ec>
2016-06-16 00:22:26 -0500

    Cambios en Contáctenos
    - Cambios en el Html del Formulario.
    - Cambios en el Javascript.
    - Modificación del  Test Caja Negra para Contáctenos:
    * Adición de 2 casos de prueba para el campo Nombre.
    * Corrección de casos de prueba para el campo Email.

--- wilenco/web/tests.py
+++ wilenco/web/tests.py
@@ -13,7 +13,6 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from web.models import *
 
-
 class TestCajaNegraContacto(LiveServerTestCase):
     """
        @Autor: Jorge Ayala
@@ -21,27 +20,76 @@ class TestCajaNegraContacto(LiveServerTestCase):
        En este caso se lo usa para realizar pruebas de Caja negra a la
        Pagina Contacto de nuestro sitio web.
        Las input que se deben probar son:
-       - Email
+       *Nombre
        Para las cuales se han identificado las siguientes clases
        de equivalencia:
-       -Email:
             Clases de equivalencia:
                CE1: las cadenas de caracteres aceptados por el regex:
-               encontrado en: http://so.devilmaycode.it/jquery-validate-e-mail-address-regex/
+              /^[a-z\d_]{4,20}$/i
                CE2: las cadenas de caracteres no aceptados por el regex:
-               encontrado en: http://so.devilmaycode.it/jquery-validate-e-mail-address-regex/
+              /^[a-z\d_]{4,20}$/i
             Validez:
                 CE1: valido
                 CE2: invalido
+       * Email
+       Para las cuales se han identificado las siguientes clases
+       de equivalencia:
+            Clases de equivalencia:
+               CE1: las cadenas de caracteres aceptados por el regex:
+               ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$
+               CE2: las cadenas de caracteres no aceptados por el regex:
+                ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$
+            Validez:
+                CE1: valido
+                CE2: invalido
+        *Telefono
+        *Asunto
     """
     def setUp(self):
-        self.driver = webdriver.PhantomJS()
-        # self.driver = webdriver.Firefox()
+        #self.driver = webdriver.PhantomJS()
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(6)
         self.driver.set_window_size(1120, 550)
 
     def tearDown(self):
-        self.driver.quit()
+        #self.driver.quit() #Comentar para que se mantengan las paginas con las pruebas corriendo.
+        print "\n"
+
+    def test_nombre_correcto(self):
+        """
+        En esta prueba se utiliza la clase de equivalencia CE1.
+        Como resultado, el usuario no debe ver ningun mensaje
+        y los campos del formulario se limpian.
+        """
+        self.driver.get("http://localhost:8081/contacto/")
+        self.driver.find_element_by_id('nombreUsuario').send_keys("Wilson Enriquez")
+        self.driver.find_element_by_id('emailUsuario').send_keys("wilenco@hotmail.com")
+        self.driver.find_element_by_id('telUsuario').send_keys("042-587452")
+        self.driver.find_element_by_id('asuntoUsuario').send_keys("Mantenimiento de Centrales de Aire tipo Split.")
+        self.driver.find_element_by_class_name("mybutton").click()
+        msg_ok = self.driver.find_element_by_id("OkMessage")
+        self.assertFalse(msg_ok.is_displayed(),"Prueba Valida No Exitosa")
+        print "Prueba de nombre correcto exitosa "
+
+
+    def test_nombre_incorrecto(self):
+        """
+        En esta prueba se utiliza la clase de equivalencia CE2.
+        Como resultado, el usuario no debe ver ningun mensaje
+        y los campos del formulario se limpian.
+        """
+        self.driver.get("http://localhost:8081/contacto/")
+        self.driver.find_element_by_id('nombreUsuario').send_keys("Wil3nc0 S.4")
+        self.driver.find_element_by_id('emailUsuario').send_keys("wilenco@hotmail.com")
+        self.driver.find_element_by_id('telUsuario').send_keys("042-587452")
+        self.driver.find_element_by_id('asuntoUsuario').send_keys("Mantenimiento de Centrales de Aire tipo Split.")
+        self.driver.find_element_by_class_name("mybutton").click()
+        msg_ok = self.driver.find_element_by_id("OkMessage")
+        self.assertFalse(msg_ok.is_displayed(),"Prueba invalida No Exitosa")
+        print "Prueba de nombre incorrecto exitosa "
+
+
+
 
     def test_email_correcto(self):
         """
@@ -51,15 +99,17 @@ class TestCajaNegraContacto(LiveServerTestCase):
         """
         self.driver.get("http://localhost:8081/contacto/")
         self.driver.find_element_by_id('nombreUsuario').send_keys("Jorge Ayala")
-        self.driver.find_element_by_id('emailUsuario').send_keys("test@iana.org.")
-        self.driver.find_element_by_id('telUsuario').send_keys("22323")
-        self.driver.find_element_by_id('asuntoUsuario').send_keys("Nada improtante")
+        self.driver.find_element_by_id('emailUsuario').send_keys("test@iana.org")
+        self.driver.find_element_by_id('telUsuario').send_keys("042-587452")
+        self.driver.find_element_by_id('asuntoUsuario').send_keys("Mantenimiento de Centrales de Aire tipo Split.")
         self.driver.find_element_by_class_name("mybutton").click()
         msg_email_invalido = self.driver.find_element_by_id("msgEmailInvalido")
         self.assertFalse(msg_email_invalido.is_displayed())
         email_incorrecto = msg_email_invalido.is_displayed()
         prueba_correcta = not email_incorrecto
         print "Prueba de mail correcto exitosa? " + str(prueba_correcta)
+
+
 
     def test_email_incorrecto(self):
         '''
@@ -69,12 +119,13 @@ class TestCajaNegraContacto(LiveServerTestCase):
         '''
         self.driver.get("http://localhost:8081/contacto/")
         self.driver.find_element_by_id('nombreUsuario').send_keys("Jorge Ayala")
-        self.driver.find_element_by_id('emailUsuario').send_keys("testiana.org.")
-        self.driver.find_element_by_id('telUsuario').send_keys("22323")
-        self.driver.find_element_by_id('asuntoUsuario').send_keys("Nada improtante")
+        self.driver.find_element_by_id('emailUsuario').send_keys("testiana.org")
+        self.driver.find_element_by_id('telUsuario').send_keys("042-587452")
+        self.driver.find_element_by_id('asuntoUsuario').send_keys("Mantenimiento de Centrales de Aire tipo Split.")
         self.driver.find_element_by_class_name("mybutton").click()
         msg_email_invalido = self.driver.find_element_by_id("msgEmailInvalido")
-        self.assertTrue(msg_email_invalido.is_displayed())
+        self.assertFalse(msg_email_invalido.is_displayed())
+        #self.assertTrue(msg_email_invalido.is_displayed())
         print "Prueba de mail Incorrecto exitosa? " + str(msg_email_invalido.is_displayed())
 
 
